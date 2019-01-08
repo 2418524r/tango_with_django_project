c896634080de828379900468dab9cb524adc91a0
LR <2418524r@student.gla.ac.uk>
2019-01-08 20:01:45 +0000

    Added about app

--- twd_project/twd_project/urls.py
+++ twd_project/twd_project/urls.py
@@ -19,8 +19,7 @@ from rango import views
 from django.conf.urls import include
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
-
 ]
